5dafc1b546aa7fe79e4bac06c2147d07a1c35fe0
Michael Windell <mike.windell@twentysixdigital.com>
2021-09-23 11:33:49 +0100

    check bemlintResult is not undefined

--- index.js
+++ index.js
@@ -6,37 +6,38 @@ const PluginError = require('plugin-error');
 let hasErrors = false;
 
 const bemlintHtml = () => {
-
   const lintFile = (file, _, cb) => {
-
     if (file.isNull()) {
       cb(null, file);
-
     } else if (file.isStream()) {
-      this.emit('error', new PluginError('gulp-bemlint-html', 'Streams not supported!'));
+      this.emit(
+        'error',
+        new PluginError('gulp-bemlint-html', 'Streams not supported!')
+      );
       cb(null, file);
-
     } else if (file.isBuffer()) {
       const bemlintResult = bemlint.verify(String(file.contents));
 
-      if (bemlintResult.length) {
+      if (bemlintResult && bemlintResult.length) {
         if (!hasErrors) {
           console.log('\n');
           hasErrors = true;
         }
 
         console.log(path.resolve(__dirname, file.relative));
-        bemlintResult.forEach(error => {
-          console.log(`    ${error.line}:${error.column}  warning  ${error.message}  ${error.ruleId}`);
+        bemlintResult.forEach((error) => {
+          console.log(
+            `    ${error.line}:${error.column}  warning  ${error.message}  ${error.ruleId}`
+          );
         });
         console.log('\n');
       }
 
       cb(null, file);
     }
-  }
+  };
 
   return through.obj(lintFile);
-}
+};
 
 module.exports = bemlintHtml;
